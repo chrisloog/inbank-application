c02af1eec1daac898004ddcfe1d8edbfc8042c10
Chris <chrisloog120@gmail.com>
2023-03-23 19:58:46 +0200

    Refactor: improve GUI design

--- src/GUI.py
+++ src/GUI.py
@@ -1,43 +1,51 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
 import loan_decision_engine
 
-
-def main():
-    window = tkinter.Tk()
-
-    window.title("Loan Decision Engine")
-    window.geometry('800x400')
-
-    tkinter.Label(window, text='Personal code').grid(row=0)
-    tkinter.Label(window, text='Loan amount').grid(row=1)
-    tkinter.Label(window, text='Loan period').grid(row=2)
-    tkinter.Label(window, text='Result').grid(row=3)
-
-    personal_code = tkinter.Entry(window, width=15)
-    personal_code.grid(column=1, row=0)
-    personal_code.focus()  # Set focus to entry widget
-
-    loan_amount = tkinter.Entry(window, width=15)
-    loan_amount.grid(column=1, row=1)
-
-    loan_period = tkinter.Entry(window, width=15)
-    loan_period.grid(column=1, row=2)
-
-    result = tkinter.Entry(window, width=30)
-    result.grid(column=1, row=3)
-
-    def clicked():
-        p_code = int(personal_code.get())
-        l_amount = int(loan_amount.get())
-        l_period = int(loan_period.get())
-        output = loan_decision_engine.decision_engine(p_code, l_amount, l_period)
-        result.insert(0, output)
-
-    btn = tkinter.Button(window, text="Click Me", command=clicked)
-    btn.grid(column=1, row=4)
-
-    window.mainloop()
+root = tk.Tk()
+root.title("Loan Decision Engine")
+root.geometry("400x300")
+root.resizable(False, False)
+root.configure(background="#F8F8F8")
 
 
-if __name__ == '__main__':
-    main()
+def submit():
+    personal_code = int(personal_code_entry.get())
+    loan_amount = int(loan_amount_entry.get())
+    loan_period = int(loan_period_entry.get())
+
+    decision_field.config(state="normal")
+    decision_field.delete("1.0", "end")
+    decision_field.insert("end", loan_decision_engine.decision_engine(personal_code, loan_amount, loan_period))
+    decision_field.config(state="disabled")
+
+
+personal_code_label = ttk.Label(root, text="Personal code:")
+personal_code_label.grid(row=0, column=0, padx=10, pady=10, sticky="W")
+personal_code_entry = ttk.Entry(root, width=30, font=("Helvetica", 12))
+personal_code_entry.grid(row=0, column=1, padx=10, pady=10)
+
+loan_amount_label = ttk.Label(root, text="Loan amount (€):")
+loan_amount_label.grid(row=1, column=0, padx=10, pady=10, sticky="W")
+loan_amount_entry = ttk.Entry(root, width=30, font=("Helvetica", 12))
+loan_amount_entry.grid(row=1, column=1, padx=10, pady=10)
+
+loan_period_label = ttk.Label(root, text="Loan period (months):")
+loan_period_label.grid(row=2, column=0, padx=10, pady=10, sticky="W")
+loan_period_entry = ttk.Entry(root, width=30, font=("Helvetica", 12))
+loan_period_entry.grid(row=2, column=1, padx=10, pady=10)
+
+decision_label = ttk.Label(root, text="Decision:")
+decision_label.grid(row=3, column=0, padx=10, pady=10, sticky="W")
+decision_field = tk.Text(root, height=5, width=30, font=("Helvetica", 12), state="disabled")
+decision_field.grid(row=3, column=1, padx=10, pady=10)
+
+submit_button = ttk.Button(root, text="Submit", command=submit)
+submit_button.grid(row=4, column=1, padx=10, pady=10)
+
+for i in range(5):
+    root.grid_rowconfigure(i, pad=5)
+for i in range(2):
+    root.grid_columnconfigure(i, pad=5)
+
+root.mainloop()
